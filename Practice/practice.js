ef21892497ce0ecb565930dc62364cafd917d1ab
Manan Arora <arora.manan.k1234@gmail.com>
2025-07-01 22:03:17 +0530

    javascript

---
+++ Practice/practice.js
@@ -0,0 +1,6 @@
+// Write code to create this <div class="card">Hello</div> and add it to the DOM using JavaScript.
+
+let div=document.createElement('div');
+
+div.textContent="Hello";
+document.body.append(div);
